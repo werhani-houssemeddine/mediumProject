0725d02abd0bba684e65cfbdeff1991f5d2388fd
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2022-07-24 00:08:00 +0100

    I set up session middleware and config the redis db to store session

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 // local files
 const userRoutes = require('./src/routes/index');
 const connectMongo = require('./db/mongo');
+const session = require('./lib/session');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,6 +15,9 @@ const PORT = process.env.PORT || 3000;
 app.disable('x-powered-by'); //to hide from user that we use express 
 app.use(express.urlencoded({extended: true})); // to parse the body of an http request
 app.use(express.json()); // to parse a json playload
+
+//configue session
+app.use(session);
 
 //connect to mongo db
 connectMongo(process.env.MONGO_URI);
