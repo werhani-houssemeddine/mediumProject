7107dd8fcd401add7b0c881081426e3153471007
werhani-houssemeddine <109459554+werhani-houssemeddine@users.noreply.github.com>
2022-07-24 21:49:05 +0100

    Delete frontend directory

--- frontend/src/components/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default () => {
-  <>
-    <h1>Hello world</h1>
-  </>
-}
--- frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
